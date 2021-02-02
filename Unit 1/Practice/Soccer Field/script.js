7775027dad7fa3097dfb2d4c5d0dab3ba7b4509e
Ilya Tate <Itate265@west-mec.local>
2021-02-02 15:25:55 -0700

    Class 02/02/21

---
+++ Unit 1/Practice/Soccer Field/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+field.onclick = function(e) {
+    let fieldCoords = this.getBoundingClientRect();
+
+    let ballCoords = {
+        top: e.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
+        left: e.clientX - fieldCoords.left - field.clientLeft + ball.clientWidth / 2
+    }
+
+    // Precent the ball from moving out of bounds
+    if(ballCoords.top < 0) {
+        ballCoords.top = 0;
+    }
+    if(ballCoords.left < 0) {
+        ballCoords.left = 0;
+    }
+
+    if(ballCoords.left + ball.clientWidth > field.clientWidth) {
+        ballCoords.left = field.clientWidth - ball.clientWidth;
+    }
+    if (ballCoords.top + ball.clientHeight > field.clientHeight) {
+        ballCoords.top = field.clientHeight - ball.clientHeight;
+    }
+
+    ball.style.left = ballCoords.left + 'px';
+    ball.style.top = ballCoords.top + 'px';
+}
