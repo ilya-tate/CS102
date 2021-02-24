f1063816a04920c03a6953546a59a2fca3bd3d5f
Ilya Tate <Itate265@west-mec.local>
2021-02-24 15:24:49 -0700

    Class 02/24/21

--- Unit 2/Stopwatch Project/script.js
+++ Unit 2/Stopwatch Project/script.js
@@ -4,10 +4,12 @@
 $(function() {
     let toggleTimeBtn = $('.toggle-time');
     let showTimeBtn = $('.show-time');
+    let $msec = $('.msec');
     let $sec = $('.sec');
     let $min = $('.min');
     let $hour = $('.hour');
-    let timeArr = [0, 0, 0]; // hour, min, sec
+    let timeArr = [0, 0, 0, 0]; // hour, min, sec, msec
+    let msecInterval;
     let secInterval;
     let minInterval;
     let hourInterval;
@@ -17,6 +19,10 @@ $(function() {
 
 
     function startClock() {
+        msecInterval = setInterval(() => {
+            timeArr[3]++;
+        }, 10);
+
         secInterval = setInterval(() => {
             timeArr[2]++;
         }, 1000);
@@ -27,18 +33,27 @@ $(function() {
 
         hourInterval = setInterval(() => {
             timeArr[0]++;
-        }, 360000);
+        }, 3600000);
 
         displayChangingTime()
     }
 
     function displayChangingTime() {
         checkingInterval = setInterval(() => {
+            if (timeArr[3] < 10) {
+                $msec.text(`0${timeArr[3]}`);
+            } else if (timeArr[3] < 100) {
+                $msec.text(`${timeArr[3]}`);
+            } else if (timeArr[3] >= 100) {
+                timeArr[3] = 0;
+                $msec.text(`0${timeArr[3]}`);
+            }
+
             if (timeArr[2] < 10) {
                 $sec.text(`0${timeArr[2]}`);
             } else if (timeArr[2] < 60) {
                 $sec.text(`${timeArr[2]}`);
-            } else if (timeArr[2] == 60) {
+            } else if (timeArr[2] >= 60) {
                 timeArr[2] = 0;
                 $sec.text(`0${timeArr[2]}`);
             }
@@ -47,21 +62,25 @@ $(function() {
                 $min.text(`0${timeArr[1]}`);
             } else if (timeArr[1] < 60) {
                 $min.text(`${timeArr[1]}`);
-            } else if (timeArr[1] == 60) {
+            } else if (timeArr[1] >= 60) {
+                timeArr[1] = 0;
                 $min.text(`0${timeArr[1]}`);
             }
 
             $hour.text(`${timeArr[0]}`);
-        }, 100);
+        }, 1);
 
         isChangingTimeRunning = true;
     }
 
     function stopClock() {
+        clearInterval(checkingInterval);
+        clearInterval(msecInterval);
         clearInterval(secInterval);
         clearInterval(minInterval);
         clearInterval(hourInterval);
-        timeArr = [0, 0, 0];
+        timeArr = [0, 0, 0, 0];
+        $msec.text('00');
         $sec.text('00');
         $min.text('00');
         $hour.text('0');
