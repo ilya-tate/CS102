17f4a18c0c6bb838863b7c0539b49516e0cca986
Ilya Tate <Itate265@west-mec.local>
2021-04-01 14:06:10 -0700

    Class 04/01/21

--- Unit 3/Extra Practice Work/script.js
+++ Unit 3/Extra Practice Work/script.js
@@ -1,10 +1,17 @@
-let input = [2, 3, 1, 1, 4];
+let input = [1, 2, 3, 4, 5];
 
 
 function calcMinJumps(arr) {
     let jumpCnt = 0;
+    let tempArr = [];
+    let indexArr = [0];
+    let valArr = [arr[0]];
 
-    // IDK HELP
+    for (i in indexArr) {
+        for (let j = 0; j <= valArr[i]; j++) {
+            tempArr.push(indexArr[i] + j);
+        }
+    }
 
     return jumpCnt;
 }
